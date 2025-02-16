035df86384e0be761f8cf8a2c2af25856a902e13
Yuki <katay.eho@gmail.com>
2025-02-16 13:57:49 +0200

    Java: T5 update - Added crossplatform support

--- T5/Today/src/main/java/tamk/ohsyte/Today.java
+++ T5/Today/src/main/java/tamk/ohsyte/Today.java
@@ -5,12 +5,25 @@ import java.util.Collections;
 import java.util.List;
 
 public class Today {
+
+    private static String OS = System.getProperty("os.name").toLowerCase();
     public static void main(String[] args) {
-        // Replace with a valid path to the events.csv file on your own computer!
 
-        String userprofile = System.getenv("USERPROFILE");
+        String userprofile = null;
+        String fileName = null;
 
-        final String fileName = userprofile + "/.today/events.csv";
+
+        if (isWindows()) {
+            userprofile = System.getenv("USERPROFILE");
+            fileName = userprofile + "/.today/events.csv";
+        } else if (isMac()) {
+            userprofile = System.getenv("HOME");
+            fileName = userprofile + "/.today/events.csv";
+        } else if (isUnix()) {
+            userprofile = System.getenv("HOME");
+            fileName = userprofile + "/.today/events.csv";
+        }
+
 
         // Exception is handled in CSVEventProvider.java
         // No additional exception handling needed
@@ -27,4 +40,17 @@ public class Today {
             System.out.println(event);
         }
     }
+
+    public static boolean isWindows() {
+        return (OS.indexOf("win") >= 0);
+    }
+
+    public static boolean isMac() {
+        return (OS.indexOf("mac") >= 0);
+    }
+
+    public static boolean isUnix() {
+        return (OS.indexOf("nix") >= 0 || OS.indexOf("nux") >= 0 || OS.indexOf("aix") > 0 );
+    }
+
 }
